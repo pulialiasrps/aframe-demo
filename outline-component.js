e3d85dc813e84756dfe7a5fc641b581a777b8c5e
Glitch (adhesive-cupboard) <none>
2018-04-28 09:01:54 +0000

    🕋📚 Checkpoint ./index.html:1023065/45:1023140/55 ./outline-component.js:1023065/266:1023140/2 ./.glitch-assets:1023140/2743

---
+++ outline-component.js
@@ -0,0 +1,10 @@
+// Registering component in outline-component.js
+AFRAME.registerComponent('outline', {
+  schema: {},
+  init: function () {},
+  update: function () {},
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+});
